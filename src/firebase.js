7b6c90b6afcb29d3e2530df2dd2e48558d3dc8d5
Faizan Raza <faizanms025347@gmail.com>
2023-11-24 20:55:12 +0530

    updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyApp-RTYZ81ey16e6o3xOEsaeLWgqsJznQ",
-    authDomain: "collab-hub-86438.firebaseapp.com",
-    projectId: "collab-hub-86438",
-    storageBucket: "collab-hub-86438.appspot.com",
-    messagingSenderId: "997870717373",
-    appId: "1:997870717373:web:4c3b17bd2e3c9c535cead4"
-  };
+  apiKey: "AIzaSyApp-RTYZ81ey16e6o3xOEsaeLWgqsJznQ",
+  authDomain: "collab-hub-86438.firebaseapp.com",
+  databaseURL: "https://collab-hub-86438-default-rtdb.firebaseio.com",
+  projectId: "collab-hub-86438",
+  storageBucket: "collab-hub-86438.appspot.com",
+  messagingSenderId: "997870717373",
+  appId: "1:997870717373:web:4c3b17bd2e3c9c535cead4"
+};
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
